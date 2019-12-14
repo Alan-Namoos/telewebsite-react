865b1e2f114665df9fa8a098466ce9ede06f0710
Alan Namoos <alan.namoos@gmail.com>
2019-12-14 06:57:56 -0800

    enhancing RN Assignment From

--- src/components/rnAssignmentSheet/RnAssignmentForm3.js
+++ src/components/rnAssignmentSheet/RnAssignmentForm3.js
@@ -5,89 +5,114 @@ import sortBy from 'lodash/sortBy';
 
 const RnAssignmentForm = () => {
 	const [floorRooms, setFloorRooms] = useState(fourthFloorRooms());
-	const [theRn, setTheRn] = useState({ name: '', number: '' });
-	const [rooms, setRooms] = useState({
+	const [currentRN, setCurrentRN] = useState({
+		name: '',
+		number: '',
+		isHidden: false
+	});
+	const [assignment, setAssignment] = useState({
 		room: '',
 		rn: '',
-		number: ''
+		number: '',
+		isHidden: true
 	});
 
 	const [roomsAndRn, setRoomsAndRn] = useState([]);
 
 	const handleChange = (e) => {
-		setRooms({ [e.target.name]: e.target.value, rn: theRn.name, number: theRn.number });
-		// console.log(rooms);
+		setAssignment({
+			[e.target.name]: e.target.value,
+			rn: currentRN.name,
+			number: currentRN.number
+		});
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		setRoomsAndRn([...roomsAndRn, rooms]);
-		setRooms({
+		setRoomsAndRn([...roomsAndRn, assignment]);
+		setAssignment({
 			room: '',
 			rn: '',
 			number: ''
 		});
 
 		const updatedFloorRooms = floorRooms.filter((room) => {
-			return rooms.room !== room;
+			return assignment.room !== room;
 		});
 		setFloorRooms(updatedFloorRooms);
 	};
 
-	// useEffect(() => {}, [rooms]);
-
-	// useEffect(() => {
-	// 	console.log('Rooms: ', rooms);
-	// 	console.log('RN and Rooms: ', roomsAndRn);
-	// }, [rooms, roomsAndRn]);
+	useEffect(() => {}, []);
 
 	return (
 		<Container>
 			<Row>
 				<Col>
-					<Form onSubmit={handleSubmit}>
-						{/* <p>
-							{theRn.name}
-							{'-'}
-							{theRn.number}
-							{' - ['}
-							{rooms.room1}
-							{']'} {'- ['}
-							{rooms.room2}
-							{']'} {'- ['}
-							{rooms.room3}
-							{']'} {'- ['}
-							{rooms.room4}
-							{']'}
-						</p> */}
+					<Row className='text-center'>
+						<Col>
+							<h4>{currentRN.name}</h4>
+						</Col>
+						<Col>
+							<h4>{currentRN.number}</h4>
+						</Col>
+						<Col>
+							{/* <h4>{assignment.roomsAssigned === 0 ? '' : `Rooms: ${assignment.roomsAssigned}`}</h4> */}
+						</Col>
+					</Row>
+					<Form
+						onSubmit={(e) => {
+							e.preventDefault();
+							console.log('RN Form submitted');
+						}}
+					>
 						<Form.Group>
-							<Form.Label>RN</Form.Label>
+							<Form.Label hidden={currentRN.isHidden}>RN</Form.Label>
 							<Form.Control
 								type='text'
 								name='name'
-								value={theRn.name}
-								onChange={(e) => setTheRn({ ...theRn, [e.target.name]: e.target.value })}
+								value={currentRN.name}
+								onChange={(e) => setCurrentRN({ ...currentRN, [e.target.name]: e.target.value })}
+								hidden={currentRN.isHidden}
+								required
 							/>
 						</Form.Group>
 
 						<Form.Group>
-							<Form.Label>RN Number</Form.Label>
+							<Form.Label hidden={currentRN.isHidden}>RN Number</Form.Label>
 							<Form.Control
 								type='text'
 								name='number'
-								value={theRn.number}
-								onChange={(e) => setTheRn({ ...theRn, [e.target.name]: e.target.value })}
+								value={currentRN.number}
+								onChange={(e) => setCurrentRN({ ...currentRN, [e.target.name]: e.target.value })}
+								hidden={currentRN.isHidden}
+								required
 							/>
 						</Form.Group>
+						<Button
+							variant='primary'
+							type='submit'
+							onClick={() => {
+								if (currentRN.name !== '' && currentRN.number !== '') {
+									setCurrentRN({ ...currentRN, isHidden: true });
+									setAssignment({ ...assignment, isHidden: false });
+								}
+							}}
+							hidden={currentRN.isHidden}
+						>
+							Add RN
+						</Button>
+					</Form>
 
-						<Form.Group controlId='exampleForm.ControlSelect1'>
-							<Form.Label>Select Room:</Form.Label>
+					<Form onSubmit={handleSubmit}>
+						<Form.Group>
+							<Form.Label hidden={assignment.isHidden}>Select Room:</Form.Label>
 							<Form.Control
 								as='select'
-								required
 								name='room'
-								value={rooms.room}
+								value={assignment.room}
 								onChange={handleChange}
+								required
+								hidden={assignment.isHidden}
 							>
 								<option value=''>No room selected</option>
 								{floorRooms.map((room, i) => {
@@ -100,41 +125,34 @@ const RnAssignmentForm = () => {
 							</Form.Control>
 						</Form.Group>
 
-						{/* <Form.Group>
-							<Form.Label>Room 1</Form.Label>
-							<Form.Control type='text' name='room' value={rooms.room} onChange={handleChange} />
-						</Form.Group> */}
-
-						{/* <Form.Group>
-							<Form.Label>Room 2</Form.Label>
-							<Form.Control type='text' name='room2' value={rooms.room2} onChange={handleChange} />
-						</Form.Group>
-
-						<Form.Group>
-							<Form.Label>Room 3</Form.Label>
-							<Form.Control type='text' name='room3' value={rooms.room3} onChange={handleChange} />
-						</Form.Group>
-
-						<Form.Group>
-							<Form.Label>Room 4</Form.Label>
-							<Form.Control type='text' name='room4' value={rooms.room4} onChange={handleChange} />
-						</Form.Group> */}
-
-						<Button variant='primary' type='submit'>
-							Submit
-						</Button>
+						<Row className='text-center'>
+							<Col>
+								<Button variant='primary' type='submit' hidden={assignment.isHidden}>
+									Add Room
+								</Button>
+							</Col>
+							<Col>
+								<Button
+									variant='primary'
+									hidden={assignment.isHidden}
+									onClick={() => {
+										setAssignment({ ...assignment, isHidden: true });
+										setCurrentRN({ name: '', number: '', roomsAssigned: 0, isHidden: false });
+									}}
+								>
+									New RN
+								</Button>
+							</Col>
+						</Row>
 					</Form>
 				</Col>
 				<Col>
-					<Table bordered size='sm'>
+					<Table bordered striped size='sm'>
 						<thead>
 							<tr>
 								<th>Room #</th>
 								<th>RN</th>
 								<th>RN #</th>
-								{/* <th>Room2</th>
-								<th>Room3</th>
-								<th>Room4</th> */}
 							</tr>
 						</thead>
 						<tbody>
@@ -144,9 +162,6 @@ const RnAssignmentForm = () => {
 										<td>{item.room}</td>
 										<td>{item.rn}</td>
 										<td>{item.number}</td>
-										{/* <td>{item.room2}</td>
-										<td>{item.room3}</td>
-										<td>{item.room4}</td> */}
 									</tr>
 								);
 							})}
